4a6d30a6bf2968c6b4492ede72b0a78095c03807
0x3ashry <47285449+0x3ashry@users.noreply.github.com>
2021-03-24 16:52:32 +0200

    Update MAC_Changer.py

--- MAC_Changer.py
+++ MAC_Changer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import subprocess
 import optparse
+import re
 
 def get_args():
     parser = optparse.OptionParser()
@@ -13,13 +14,29 @@ def get_args():
         parser.error("[-] Please specify a new MAC address, use --help for more info")
     return options
 
-
 def change_mac(interface,new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(b"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        return "[-] Couldn't find a MAC address."
+
 
 options = get_args()
+current_mac = get_current_mac(options.interface).decode()
+print("Current MAC = " + str(current_mac))
+
 change_mac(options.interface,options.new_mac)
+
+current_mac = get_current_mac(options.interface).decode()
+if current_mac == options.new_mac:
+    print("[+] MAC address has changed successfully.")
+else:
+    print("[+] MAC address failed to change.")
